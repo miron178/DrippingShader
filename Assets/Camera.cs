1c433c55813b3d431ac8792589513410733e9476
Miron Bury <miron.bury@gmail.com>
2023-01-09 00:33:08 +0000

    new camera

---
+++ Assets/Camera.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    float distance;
+
+    [SerializeField]
+    Transform far;
+
+    [SerializeField]
+    Transform near;
+
+    private void Update()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        distance += scroll;
+        Mathf.Clamp(distance, 0, 1);
+
+        transform.position = Vector3.Lerp(far.position, near.position, distance);
+    }
+
+}
